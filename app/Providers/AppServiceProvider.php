17c6bb912f35a56bddcb21a7e35eb36677651ce3
rickycabugawan <34016053+rickycabugawan@users.noreply.github.com>
2018-03-06 22:20:22 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -61,9 +61,6 @@ class AppServiceProvider extends ServiceProvider
             'most_recent_comments',
             'random_posts',
             'archives'));
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 
     /**
